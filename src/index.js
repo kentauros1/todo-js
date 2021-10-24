6aa7f60a2182ea74836769ac2d37492145484bb6
kentauros1 <48154351+kentauros1@users.noreply.github.com>
2021-10-24 22:16:02 +0900

    テキストの追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキスト入力値の取得＆初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //liタグの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //divタグの子要素に各要素を追加
+  div.appendChild(li);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+//add-buttonIDにクリックイベントを付加
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
