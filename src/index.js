3f42a12e82b932513f8d231f54b03d48852faf83
kentauros1 <48154351+kentauros1@users.noreply.github.com>
2021-10-24 22:22:12 +0900

    タスクの追加（ボタン）

--- src/index.js
+++ src/index.js
@@ -12,8 +12,24 @@ const onClickAdd = () => {
   //divの生成
   const div = document.createElement("div");
   div.className = "list-row";
+
+  //完了ボタンの作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //削除ボタンの作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
   //divタグの子要素に各要素を追加
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
